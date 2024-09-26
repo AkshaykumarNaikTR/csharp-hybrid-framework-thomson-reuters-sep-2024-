9e6aff664d333f75080a31bb04b7b68982605831
AkshaykumarNaikTR <164191843+AkshaykumarNaikTR@users.noreply.github.com>
2024-09-26 08:39:23 +0530

    Add files via upload

---
+++ LoginUITest.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThomsonReuters.MedicalRecordAutomation.Base;
+
+namespace ThomsonReuters.MedicalRecordAutomation
+{
+    /// <summary>
+    /// Login UI Test Cases
+    /// </summary>
+    public class LoginUITest : AutomationWrapper
+    {
+        [Test]
+        public void TitleTest()
+        {
+            string actualTitle = driver.Title;
+            Assert.That(actualTitle, Is.EqualTo("OpenEMR Login"));
+        }
+
+        [Test]
+        public void ApplicationDescriptionTest()
+        {
+            //Assert the text --> "The most popular open-source Electronic Health Record and Medical Practice Management solution."
+            string actualDescription = driver.FindElement(By.XPath("//p[contains(text(),'most')]")).Text;
+            Assert.That(actualDescription, Is.EqualTo("The most popular open-source Electronic Health Record and Medical Practice Management solution."));
+        }
+    }
+}
